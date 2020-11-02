58d18ba41d20f88526213ae01681f8e47dff0944
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-11-02 04:47:40 +0100

    RWD update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,19 @@ import logo from '@/images/logo.png'
 import trump from '@/images/trump.png'
 
 const Header = (props) => {
+  const toggleNav = (event) => {
+    document.querySelector('.sidebar').classList.toggle('show')
+    event.target.classList.toggle('show')
+  }
+
   return (
     <header>
       <div id="left">
-        <div className="hamburger"></div>
-        <a href="/h">
+        <div className="hamwrapper" onClick={toggleNav}>
+          <div className="hamburger"></div>
+        </div>
+
+        <a href="/">
           <img src={logo} title="FastFile" alt="FastFile" />
         </a>
       </div>
