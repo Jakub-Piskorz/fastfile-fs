c5476f951a0b3834ebfd052765db5dc6cfd85e48
Mateusz <mateuszszczykutowicz@wp.pl>
2021-01-04 17:57:05 +0100

    LP - Font-size; footer; buttons

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,55 +1,56 @@
-import HtmlHead from '@/scripts/HtmlHead'
-import style from '@/landing-page.module.scss'
-import { useEffect } from 'react'
-import API from '@/scripts/API'
-import CookieScripts from '@/scripts/cookie-scripts'
+import HtmlHead from "@/scripts/HtmlHead";
+import style from "@/landing-page.module.scss";
+import { useEffect } from "react";
+import API from "@/scripts/API";
+import CookieScripts from "@/scripts/cookie-scripts";
 
 //A lot of images. Skip to line 36.
-import camera from '@/images/graphics/camera.svg'
-import code from '@/images/graphics/code.svg'
-import folder from '@/images/graphics/folder.svg'
-import jpg from '@/images/graphics/jpg.svg'
-import key from '@/images/graphics/key.svg'
-import mp3 from '@/images/graphics/mp3.svg'
-import pdf from '@/images/graphics/pdf.svg'
-import plusCircle from '@/images/graphics/plus-circle.svg'
-import search from '@/images/graphics/search.svg'
-import share from '@/images/graphics/share.svg'
-import userGroup from '@/images/graphics/user-group.svg'
-import appStoreBtn from '@/images/icons/app-store-btn.png'
-import facebookIcon from '@/images/icons/facebook-icon.png'
-import facebookRoundIcon from '@/images/icons/facebook-round-icon.png'
-import githubRoundedIcon from '@/images/icons/github-rounded-icon.png'
-import googleRoundedIcon from '@/images/icons/google-rounded-icon.png'
-import linkedInIcon from '@/images/icons/linkedin-icon.png'
-import playStoreBtn from '@/images/icons/play-store-btn.png'
-import securityIcon from '@/images/icons/security-icon.png'
-import shape1 from '@/images/icons/shape1.svg'
-import shape2 from '@/images/icons/shape2.svg'
-import twitterIcon from '@/images/icons/twitter-icon.png'
-import ytIcon from '@/images/icons/yt-icon.png'
-import fastfileBlack from '@/images/logo/FastFile-black.jpg'
-import fastfileMockup from '@/images/logo/FastFile-mockup.jpg'
-import fastfileReverse from '@/images/logo/FastFile-reverse.png'
-import fastfileReverse2 from '@/images/logo/FastFile-reverse.jpg'
-import fastfileWeb from '@/images/logo/FastFile-web.png'
-import fastfile from '@/images/logo/FastFile.jpg'
-import fastfile2 from '@/images/logo/FastFile.png'
+import camera from "@/images/graphics/camera.svg";
+import code from "@/images/graphics/code.svg";
+import folder from "@/images/graphics/folder.svg";
+import jpg from "@/images/graphics/jpg.svg";
+import key from "@/images/graphics/key.svg";
+import mp3 from "@/images/graphics/mp3.svg";
+import pdf from "@/images/graphics/pdf.svg";
+import plusCircle from "@/images/graphics/plus-circle.svg";
+import search from "@/images/graphics/search.svg";
+import share from "@/images/graphics/share.svg";
+import userGroup from "@/images/graphics/user-group.svg";
+import appStoreBtn from "@/images/icons/app-store-btn.png";
+import facebookIcon from "@/images/icons/facebook-icon.png";
+import facebookRoundIcon from "@/images/icons/facebook-round-icon.png";
+import githubRoundedIcon from "@/images/icons/github-rounded-icon.png";
+import googleRoundedIcon from "@/images/icons/google-rounded-icon.png";
+import linkedInIcon from "@/images/icons/linkedin-icon.png";
+import playStoreBtn from "@/images/icons/play-store-btn.png";
+import securityIcon from "@/images/icons/security-icon.png";
+import shape1 from "@/images/icons/shape1.svg";
+import shape2 from "@/images/icons/shape2.svg";
+import twitterIcon from "@/images/icons/twitter-icon.png";
+import ytIcon from "@/images/icons/yt-icon.png";
+import fastfileBlack from "@/images/logo/FastFile-black.jpg";
+import fastfileMockup from "@/images/logo/FastFile-mockup.jpg";
+import fastfileReverse from "@/images/logo/FastFile-reverse.png";
+import fastfileReverse2 from "@/images/logo/FastFile-reverse.jpg";
+import fastfileWeb from "@/images/logo/FastFile-web.png";
+import fastfile from "@/images/logo/FastFile.jpg";
+import fastfile2 from "@/images/logo/FastFile.png";
 
 const LandingPage = (props) => {
   const login = (e) => {
-    e.preventDefault()
-    const username = document.querySelector('#' + style.username).value
-    const password = document.querySelector('#' + style.password).value
+    e.preventDefault();
+    const username = document.querySelector("#" + style.username).value;
+    const password = document.querySelector("#" + style.password).value;
     API.login(username, password).then((result) => {
-      CookieScripts.add('token', result.token)
-      if (CookieScripts.value) window.location.href = '/'
-    })
-  }
+      CookieScripts.add("token", result.token);
+      if (CookieScripts.value) window.location.href = "/";
+    });
+  };
 
   return (
     <>
       <HtmlHead title="FastFile | Home" />
+
       {/* <!-- ? Header ? --> */}
       <header className={style.header}>
         <img src={fastfileReverse} alt="FastFile" className={style.logo} />
@@ -78,9 +79,9 @@ const LandingPage = (props) => {
       <main>
         {/* <!-- ? Header ? --> */}
         <section className={style.first}>
-          <div className={style['header__wrapper']}>
+          <div className={style["header__wrapper"]}>
             <h1>
-              <span className={style['main__header__h2']}>Send Files</span>{' '}
+              <span className={style["main__header__h2"]}>Send Files</span>{" "}
               Everywhere
             </h1>
 
@@ -117,7 +118,7 @@ const LandingPage = (props) => {
                   id={style.submit}
                   onClick={login}
                 />
-                <button type="button" id={style['login__sign_up']}>
+                <button type="button" id={style["login__sign_up"]}>
                   Sign Up
                 </button>
               </label>
@@ -125,7 +126,7 @@ const LandingPage = (props) => {
 
             <p>Or sign up with another account</p>
 
-            <div className={style['login__another_login__wrapper']}>
+            <div className={style["login__another_login__wrapper"]}>
               <a href="#">
                 <img src={googleRoundedIcon} alt="Google" />
               </a>
@@ -141,7 +142,7 @@ const LandingPage = (props) => {
 
         {/* <!-- ? Secure ? --> */}
         <section className={style.secure}>
-          <div className={style['secure__wrapper']}>
+          <div className={style["secure__wrapper"]}>
             <h2>
               Keep Your <span>Data Secure</span>
             </h2>
@@ -151,14 +152,14 @@ const LandingPage = (props) => {
             <img
               src={securityIcon}
               alt="secure"
-              className={style['security_icon']}
+              className={style["security_icon"]}
             />
             <img
               src="#"
               alt="secure your device"
-              className={style['secure-icon']}
+              className={style["secure-icon"]}
             />
-            <div className={style['secure__text__wrapper']}>
+            <div className={style["secure__text__wrapper"]}>
               <p>
                 FastFile will give you full control over your most vulnerable
                 data, that you can access all around the world
@@ -168,7 +169,7 @@ const LandingPage = (props) => {
                 Google Play
               </p>
             </div>
-            <div className={style['secure__store__wrapper']}>
+            <div className={style["secure__store__wrapper"]}>
               <a href="#">
                 <img
                   src={appStoreBtn}
@@ -200,8 +201,8 @@ const LandingPage = (props) => {
       {/* <!-- ? Footer ? --> */}
       <footer>
         <img className={style.logo} src={fastfileReverse} alt="" />
-        <div className={style['footer__wrapper']}>
-          <article className={style['about_us']}>
+        <div className={style["footer__wrapper"]}>
+          <article className={style["about_us"]}>
             <h3>About Us</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
@@ -210,9 +211,9 @@ const LandingPage = (props) => {
               id quos voluptate, voluptas incidunt numquam ut.
             </p>
           </article>
-          <article className={style['quick_links']}>
+          <article className={style["quick_links"]}>
             <h3>Quick Links</h3>
-            <nav className={style['nav__footer']}>
+            <nav className={style["nav__footer"]}>
               <ul>
                 <li>
                   <a href="#">Login</a>
@@ -232,7 +233,7 @@ const LandingPage = (props) => {
               </ul>
             </nav>
           </article>
-          <article className={style['footer__follow_us']}>
+          <article className={style["footer__follow_us"]}>
             <h3>Follow us</h3>
             <a href="#">
               <img src={facebookIcon} alt="Facebook" />
@@ -250,11 +251,11 @@ const LandingPage = (props) => {
         </div>
         <article className={style.end}>
           <h2>2020 FastFile Inc. All rights reserved.</h2>
-          <div className={style['end__wrapper']}>
+          <div className={style["end__wrapper"]}>
             <a href="#">Privacy Policy</a>
-            <span>|</span>
+            <span> | </span>
             <a href="#">Terms & Conditions</a>
-            <span>|</span>
+            <span> | </span>
             <a href="#">Cookies</a>
           </div>
         </article>
@@ -264,7 +265,7 @@ const LandingPage = (props) => {
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
