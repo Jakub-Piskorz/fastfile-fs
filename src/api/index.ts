9139613b7db6f8ceec4bd3cda1f76b4cf4a470d8
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2026-08-20 18:39:26 +0200

    fix: backend URL on local dev

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,9 +6,15 @@ export * from '@/api/utils'
 
 export const getToken = (): string | undefined => CookieScripts.get('token')
 
+// @ts-ignore Webpack knows what "process" is
+const isDevServer = process.env.NODE_ENV === 'development'
+
 export const swaggerApi = new SwaggerApi({
   headers: {
     Authorization: `Bearer ${getToken()}`
-  }
+  },
+  baseURL:
+    isDevServer ? 'https://localhost:8080' : 'https://jakubpiskorz.dev:8080'
 })
+
 export default swaggerApi
